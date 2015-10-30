f48bb9afdec8c3725135cb2468c54e685efd6592
babalugats76 <james@colestock.com>
2015-10-30 01:14:37 -0600

    Finished Part 3 of the tutorial

--- conf.lua
+++ conf.lua
@@ -4,5 +4,5 @@ function love.conf(t)
    t.title = "My Awesome First Game"
    t.version = "0.9.2"
    t.window.width = 480
-   t.window.height = 800
+   t.window.height = 640
 end
--- main.lua
+++ main.lua
@@ -1,22 +1,41 @@
 debug = true
 
+-- Player Status
+isAlive = true
+score = 0
+
 -- Timers
 canShoot = true
 canShootTimerMax = 0.2
 canShootTimer = canShootTimerMax
+createEnemyTimerMax = 0.4
+createEnemyTimer = createEnemyTimerMax
 
 -- Image storage
 bulletImg = love.graphics.newImage('assets/bullet.png')
+enemyImg = love.graphics.newImage('assets/enemy.png')
 
 -- Bullet table
 bullets = {} -- used to keep track of the bullets we will shoot
+enemies = {} -- used to keep track of the enemies that we will spawn
 
-player = { img = nil, x = 200, y = 710, speed = 150 } -- our player table/object
+player = { img = nil, x = 200, y = 550, speed = 150 } -- our player table/object
 
 -- Called when the game first starts (load assets, etc.)
 function love.load(arg)
    -- Loads the image into memory (assigning to player object)
    player.img = love.graphics.newImage('assets/plane.png')
+end
+
+-- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
+-- Returns true if two boxes overlap, false if they don't
+-- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
+-- x2,y2,w2 & h2 are the same, but for the second box
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
 end
 
 -- Called every frame (takes delta time as an argument)
@@ -64,17 +83,78 @@ function love.update(dt)
       end
    end
 
+   -- Control spawning of enemies
+   createEnemyTimer = createEnemyTimer - (1 * dt) -- decrement timer
+   if createEnemyTimer < 0 then -- check if time to create an enemy
+      createEnemyTimer = createEnemyTimerMax -- reset timer
+      xPos = love.math.random(10, love.graphics.getWidth() - 10) -- randomly generate x-coordinate for new enemy
+      newEnemy = {x = xPos, y = -10, img = enemyImg} -- create new table for enemy to be spawned
+      table.insert(enemies,newEnemy) -- insert into the enemies table to be tracked, etc.
+   end
+
+   -- Update the enemies' positions so that they can be drawn
+   for i,enemy in ipairs(enemies) do
+       enemy.y = enemy.y + (200 * dt)
+       if enemy.y > love.graphics.getHeight() then
+          table.remove(enemies,i)
+       end
+   end
+
+   -- Check for collisions
+   for i, enemy in ipairs(enemies) do
+     for j, bullet in ipairs(bullets) do
+         if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), bullet.x, bullet.y, bullet.img:getWidth(), bullet.img:getHeight()) then
+            table.remove(enemies,i)
+            table.remove(bullets,j)
+            score = score + 1
+         end
+     end
+
+     if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight()) and isAlive then
+         table.remove(enemies,i)
+         isAlive = false
+     end
+   end
+
+   if not isAlive and love.keyboard.isDown('r') then
+       -- Remove all bullets and enemies
+       enemies = {}
+       bullets = {}
+
+       -- Reset Timers
+       createEnemyTimer = createEnemyTimerMax
+       canShootTimer = canShootTimerMax
+
+       -- Move player back to default position
+       player.x = 200
+       player.y = 550
+
+       -- Reset the game state (so we can start all over)
+       isAlive = true
+       score = 0
+
+   end
+
+
 end
--- Called every frame (takes delta time as an argument)
+
 function love.draw()
 
   -- Draw the plane at the position indicated by the table
-  love.graphics.draw(player.img, player.x, player.y) -- Draw plane at (100,100); origin point in top-left corner
+  if isAlive then
+     love.graphics.draw(player.img, player.x, player.y) -- Draw plane at (100,100); origin point in top-left corner
+  else
+     love.graphics.print("Press 'R' to restart", love.graphics:getWidth()/2-50, love.graphics:getHeight()/2-10)
+  end
 
   -- Draw the bullets at the position indicated by the table
   for i,bullet in ipairs(bullets) do
       love.graphics.draw(bullet.img, bullet.x, bullet.y)
   end
 
+  -- Draw the enemy plane but make sure to rotate 180 degrees from the center (love uses top-left corner as 0,0)
+  for i,enemy in ipairs(enemies) do
+       love.graphics.draw(enemy.img, enemy.x, enemy.y)
+  end
 
 end
